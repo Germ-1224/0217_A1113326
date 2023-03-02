8bea81889ffeb7dbba1369a7b29b7167e8b2a6a1
Germ-1224 <125894890+Germ-1224@users.noreply.github.com>
2023-03-02 21:19:14 +0800

    Create A1113326_0224_2.java

---
+++ A1113326_0224_2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class A1113326_0224_2{
+
+	public static void main(String [] argv){
+
+		System.out.println("請輸入攝氏溫度:");
+
+		Scanner a = new Scanner(System.in);
+
+		int Celsius = a.nextInt();
+
+		System.out.println("換算後的華氏溫度為:" + (Celsius * 9 / 5 + 32));
+	
+	}
+
+}
+ 
