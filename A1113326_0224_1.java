bf98ee868c0adacf708a512c96125b087ae32044
Germ-1224 <125894890+Germ-1224@users.noreply.github.com>
2023-03-02 21:17:30 +0800

    Create A1113326_0224_1.java

---
+++ A1113326_0224_1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class A1113326_0224_1{
+
+	public static void main(String [] argv){
+
+		System.out.println("請輸入一個整數:");
+
+		Scanner a = new Scanner(System.in);
+
+		int number = a.nextInt();
+
+		if(number %2 == 0){
+
+				System.out.println("此數為偶數");
+		}
+
+		else{
+
+				System.out.println("此數為奇數");
+		}
+	}
+ 
+}
